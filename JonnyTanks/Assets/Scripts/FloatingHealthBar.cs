08b29d4bfe0b27a9786acee1fc052c22c3e49432
101Titanium <133938102+101Titanium@users.noreply.github.com>
2024-01-09 09:26:15 -0800

    push

---
+++ JonnyTanks/Assets/Scripts/FloatingHealthBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingHealthBar : MonoBehaviour
+{
+
+    [SerializeField] private Slider slider;
+    [SerializeField] private Camera camera;
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject Player;
+    private Vector3 offset;
+    public float offsetNumber = 0.75f;
+
+    public string[] colorFirePointTags = { "GreenPlayer", "BluePlayer", "RedPlayer", "SandPlayer", "BlackPlayer" };
+
+    [SerializeField] private PlayerColor pickedColor;
+    public enum PlayerColor
+    {
+        GreenPlayer,
+        BluePlayer,
+        RedPlayer,
+        SandPlayer,
+        BlackPlayer,
+    }
+
+    void Start()
+    {
+        offset = new Vector3(0, offsetNumber, 0);
+
+        string selectedTag = colorFirePointTags[(int)pickedColor];
+
+        Player = GameObject.FindGameObjectWithTag(selectedTag);
+    }
+
+    public void UpdateHealthBar(float currentValue, float maxValue)
+    {
+        slider.value = currentValue / maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target.position = Player.transform.position;
+
+        transform.rotation = camera.transform.rotation;
+        transform.position = target.position + offset;
+    }
+}
---
+++ JonnyTanks/Assets/Scripts/TankHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour
+{
+
+    public float health, maxHealth = 3f;
+    public FloatingHealthBar healthBar;
+
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        healthBar.UpdateHealthBar(health, maxHealth);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
